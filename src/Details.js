35339f32564a1deaaedc3b86adf0bfde7a97430d
Rachid Benkirane <rachid.benkirane@gmail.com>
2022-09-24 12:58:19 +0200

    Error boundary added

--- src/Details.js
+++ src/Details.js
@@ -1,11 +1,10 @@
 import { Component } from "react";
 import { useParams } from "react-router-dom";
+import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
 
 class Details extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { loading: true };
-  }
+  state = { loading: true };
 
   async componentDidMount() {
     const res = await fetch(
@@ -20,10 +19,12 @@ class Details extends Component {
       return <h2>loading … </h2>;
     }
 
-    const { animal, breed, city, state, description, name } = this.state;
+    const { animal, breed, city, state, description, name, images } =
+      this.state;
 
     return (
       <div className="details">
+        <Carousel images={images} />
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} — ${breed} — ${city}, ${state}`}</h2>
@@ -37,7 +38,10 @@ class Details extends Component {
 
 const WrappedDetails = () => {
   const params = useParams();
-  return <Details params={params} />;
+  return (
+    <ErrorBoundary>
+      <Details params={params} />
+    </ErrorBoundary>
+  );
 };
-
 export default WrappedDetails;
