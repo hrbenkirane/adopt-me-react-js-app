13baf0823bfd6c2161739458823204e4b42f20ea
Rachid Benkirane <rachid.benkirane@gmail.com>
2022-09-24 11:52:42 +0200

    strict mode added to the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { render } from "react-dom";
 import SearchParams from "./SearchParams";
+import { StrictMode } from "react";
 
 const App = () => {
   return (
-    <div>
-      <SearchParams />
-    </div>
+    <StrictMode>
+      <div>
+        <h1>Adopt Me!</h1>
+        <SearchParams />
+      </div>
+    </StrictMode>
   );
 };
+
 render(<App />, document.getElementById("root"));
